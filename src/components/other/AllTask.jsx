dbafc083df1a91ca9fe946293a9e9b46a382a2fc
siddharthbankar12 <siddharthbankar1204@gmail.com>
2025-03-29 23:37:49 +0530

    updated

--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -17,9 +17,12 @@ const AllTask = () => {
       </div>
 
       <div>
-        {authData.employees.map(function (elem) {
+        {authData.employees.map(function (elem, idx) {
           return (
-            <div className="bg-gray-200 mb-2 py-2 px-4 flex justify-between rounded">
+            <div
+              key={idx}
+              className="bg-gray-200 mb-2 py-2 px-4 flex justify-between rounded"
+            >
               <h3 className="w-1/5 text-black font-bold">{elem.firstName}</h3>
               <h3 className="w-1/5 text-blue-600 font-bold text-center">
                 {elem.taskCount.newTask}
--- src/components/other/CreateTask.jsx
+++ src/components/other/CreateTask.jsx
@@ -1,36 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateTask = () => {
+  const [taskList, setTaskList] = useState([]);
+
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+    date: "",
+    assignTo: "",
+    category: "",
+  });
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    setTaskList([...taskList, task]);
+
+    setTask({
+      title: "",
+      description: "",
+      date: "",
+      assignTo: "",
+      category: "",
+    });
+  };
+
+  // console.log(taskList);
+
+  const onChangeHandler = (e) => {
+    setTask({ ...task, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="p-5 bg-[#1c1c1c] mt-7 rounded">
-      <form className="flex flex-wrap w-full items-start justify-between">
+      <form
+        onSubmit={submitHandler}
+        className="flex flex-wrap w-full items-start justify-between"
+      >
         <div className="w-1/2">
           <div>
-            <h3 className="text-sm text-gray-300 mb-0.5">Task Title</h3>
+            <h3 className="text-sm text-gray-300 mb-0.5 font-bold">
+              Task Title
+            </h3>
             <input
+              value={task.title}
+              name="title"
+              onChange={onChangeHandler}
               className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
               type="text"
               placeholder="Make a UI design"
             />
           </div>
           <div>
-            <h3 className="text-sm text-gray-300 mb-0.5">Date</h3>
+            <h3 className="text-sm text-gray-300 mb-0.5 font-bold">Date</h3>
             <input
+              value={task.date}
+              name="date"
+              onChange={onChangeHandler}
               className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
               type="date"
             />
           </div>
           <div>
-            <h3 className="text-sm text-gray-300 mb-0.5">Assign to</h3>
+            <h3 className="text-sm text-gray-300 mb-0.5 font-bold">
+              Assign to
+            </h3>
             <input
+              value={task.assignTo}
+              name="assignTo"
+              onChange={onChangeHandler}
               className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
               type="text"
               placeholder="employee name"
             />
           </div>
           <div>
-            <h3 className="text-sm text-gray-300 mb-0.5">Category</h3>
+            <h3 className="text-sm text-gray-300 mb-0.5 font-bold">Category</h3>
             <input
+              value={task.category}
+              name="category"
+              onChange={onChangeHandler}
               className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
               type="text"
               placeholder="design, dev, etc"
@@ -39,13 +88,17 @@ const CreateTask = () => {
         </div>
 
         <div className="w-2/5 flex flex-col items-start">
-          <h3 className="text-sm text-gray-300 mb-0.5">Description</h3>
+          <h3 className="text-sm text-gray-300 mb-0.5 font-bold">
+            Description
+          </h3>
           <textarea
+            value={task.description}
+            name="description"
+            onChange={onChangeHandler}
             className="text-sm py-2 px-4 w-full h-44 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
-            name=""
             id=""
           ></textarea>
-          <button className="bg-emerald-500 py-3 hover:bg-emerald-600 px-5 rounded text-sm mt-1 w-full">
+          <button className="bg-emerald-500 py-3 hover:bg-emerald-600 px-5 rounded text-base font-bold mt-1 w-full">
             Create Task
           </button>
         </div>
